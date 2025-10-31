39b375e0122dc119374c4d5f717e47f7e312410f
danzito14 <urielalonsocaro@outlook.com>
2025-10-30 21:00:52 -0700

    aqui la cosa se puso rara y valio todo
    avanzando con cuidado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,14 @@
 import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { Carrito } from './pages/carrito/carrito';
-import { User } from './pages/user/user'; //<--------
-import { Login } from './pages/login/login'; //<--------
-import { Register } from './pages/register/register'; //<--------
-import { AuthCode } from './pages/auth-code/auth-code'; //<--------
-import { AuthError } from './pages/auth-error/auth-error'; //<--------
-// import { Temporal } from './pages/temporal/temporal';
-// import { Result } from './pages/result/result';
-// import { DireccionPago } from './pages/direccion-pago/direccion-pago';
+import { User } from './pages/user/user';
+import { Login } from './pages/login/login';
+import { Register } from './pages/register/register';
+import { AuthCode } from './pages/auth-code/auth-code';
+import { AuthError } from './pages/auth-error/auth-error';
+import { Temporal } from './pages/temporal/temporal';
+import { Result } from './pages/result/result';
+import { DireccionPago } from './pages/direccion-pago/direccion-pago';
 // import { General } from './pages/general/general';
 // import { ResumenPedido } from './pages/resumen-pedido/resumen-pedido';
 
@@ -20,11 +20,11 @@ export const routes: Routes = [
     { path: 'register', component: Register },
     { path: 'auth-code', component: AuthCode },
     { path: 'auth-error', component: AuthError },
-    // { path: 'result', component: Result },
-    // { path: 'direccion-pago', component: DireccionPago },
+    { path: 'result', component: Result },
+    { path: 'direccion-pago', component: DireccionPago },
     // { path: 'general', component: General },
     // { path: 'resumen-pedido', component: ResumenPedido },
 
 
-    // { path: 'temporal', component: Temporal }
+    { path: 'temporal', component: Temporal }
 ];
