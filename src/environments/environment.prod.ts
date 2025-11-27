e8df9f9f182713ca201b8ac1b379668aee308d97
danzito14 <urielalonsocaro@outlook.com>
2025-11-27 01:20:04 -0700

    Preaprando todo para railyway 2
    Me equivoque en la url y le puse un / de más

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://breath-a-new-kindomherman-production.up.railway.app/'
+    apiUrl: 'https://breath-a-new-kindomherman-production.up.railway.app'
 };
